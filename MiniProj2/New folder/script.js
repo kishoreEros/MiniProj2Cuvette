e0b5cd71f7ec40164530e945c48237aabd500ceb
kishore lingamshetty <gajula kishore kumar>
2024-08-04 10:36:39 +0530

    first commit

---
+++ MiniProj2/New folder/script.js
@@ -0,0 +1,7 @@
+// Add any necessary JavaScript for interactivity
+document.querySelector('.menu-toggle').addEventListener('click', function() {
+    document.querySelector('nav ul').classList.toggle('show');
+    document.querySelector('.search-bar').classList.toggle('show');
+});
+
+// You might want to add more JavaScript for other interactive features
